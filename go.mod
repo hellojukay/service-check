d1fe094506b5eab44ac29e2c0b447b37db29b36f
licong <licong@qianxin.com>
2020-09-02 19:16:27 +0800

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/hellojukay/service-check
+
+go 1.15
+
+require github.com/fatih/color v1.9.0
---
+++ main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/fatih/color"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+type StringArray []string
+
+func (s *StringArray) String() string {
+	return "[" + strings.Join(*s, ",") + "]"
+}
+
+func (s *StringArray) Set(value string) error {
+	*s = append(*s, value)
+	return nil
+}
+
+func (s StringArray) Index(i int) (string, error) {
+	if i >= len(s) {
+		return "", fmt.Errorf("index out of range")
+	}
+	var arr []string
+	arr = s
+	return arr[i], nil
+}
+
+func (s StringArray) IsBoolFlag() bool {
+	return false
+}
+
+// Service 系统服务
+type Service interface {
+	IsRunning() bool
+	IsEnabled() bool
+}
+
+// Unit systemd Unit
+type Unit struct {
+	Name  string
+	exsit bool
+}
+
+// IsRunning systemd check Unit is active
+func (unit Unit) IsRunning() bool {
+	if !unit.exsit {
+		return false
+	}
+	return run([]string{"systemctl", "is-active", unit.Name})
+}
+
+// Check systemd Unit is enabled
+func (unit Unit) IsEnabled() bool {
+	if !unit.exsit {
+		return false
+	}
+	return run([]string{"systemctl", "is-enabled", unit.Name})
+}
+
+func (unit Unit) Exsit() bool {
+	return unit.exsit
+}
+
+// NewUnit create a Unit
+func NewService(name string) Service {
+	var u = Unit{
+		Name: name,
+	}
+	u.exsit = run([]string{"systemctl", "status", name})
+	return u
+}
+
+func run(cmds []string) bool {
+	var cmd = exec.Command("sudo", cmds...)
+	if cmd.Run() != nil {
+		return false
+	}
+	return cmd.ProcessState.ExitCode() == 0
+}
+
+var (
+	RUNNING  = color.GreenString("active")
+	FAILED   = color.RedString("inactive")
+	ENABLEED = color.GreenString("enabled")
+	DISABLED = color.RedString("diabled")
+)
+var allService StringArray
+
+func init() {
+	flag.Var(&allService, "s", "all services you  want to check")
+	flag.Parse()
+}
+
+var printFmt = "%-20s%-10s%-10s\n"
+
+func main() {
+	if len([]string(allService)) == 0 {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	fmt.Printf(printFmt, "SERVICE", "ENABLED", "STATUS")
+	for _, service := range []string(allService) {
+		var unit = NewService(service)
+		if unit.IsEnabled() && unit.IsRunning() {
+			fmt.Printf(printFmt, service, "active", "enabled")
+			continue
+		}
+		if unit.IsEnabled() && (!unit.IsRunning()) {
+			fmt.Printf(printFmt, service, "inactive", "enabled")
+			continue
+		}
+		if !unit.IsEnabled() && unit.IsRunning() {
+			fmt.Printf(printFmt, service, "active", "disabled")
+			continue
+		}
+		if !unit.IsEnabled() && !unit.IsRunning() {
+			fmt.Printf(printFmt, service, "inactive", "disabled")
+			continue
+		}
+	}
+}
